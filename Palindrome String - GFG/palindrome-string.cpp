a875c560533d9f94293b084fef8be62985ad1dfc
Purnita Sharma <95404558+PurnitaSharma@users.noreply.github.com>
2023-08-25 14:09:25 +0530

    Added solution - LeetHub

--- Palindrome String - GFG/palindrome-string.cpp
+++ Palindrome String - GFG/palindrome-string.cpp
@@ -12,7 +12,7 @@ public:
 	{
 	    // Your code goes here
 	    int n = s.length();
-	    for(int i=0;i<n/2;i++)
+	    for(int i=0;i<=n/2;i++)
 	    {
 	        if(s[i] != s[n-i-1]) return false;
 	    }
