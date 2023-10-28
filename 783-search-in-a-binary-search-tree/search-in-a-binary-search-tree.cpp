0464cae40bfeba5695455513e71807a9082f3d20
Purnita Sharma <95404558+PurnitaSharma@users.noreply.github.com>
2023-10-28 18:33:50 +0530

    Time: 38 ms (16.59%) | Memory: 35.3 MB (6.35%) - LeetSync

--- 783-search-in-a-binary-search-tree/search-in-a-binary-search-tree.cpp
+++ 783-search-in-a-binary-search-tree/search-in-a-binary-search-tree.cpp
@@ -14,7 +14,7 @@ public:
     TreeNode* searchBST(TreeNode* root, int val) {
         if(root == NULL) return NULL;
         if(root->val == val) return root;
-        if(root->val > val) return searchBST(root->left , val);
+        if(root->val > val)  return searchBST(root->left , val);
         else return searchBST(root->right , val);
     }
 };
